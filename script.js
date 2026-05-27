d7c16fa39df1fad9a2eacd24f54300fdd06b3d8c
Nassim <achnassim34@gmail.com>
2026-05-27 11:31:02 +0200

    <Feat> Multiplicateur fonctionnel

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const cookieBtn = document.getElementById("click")
-const cookieCount = document.getElementById("cookie-count")
+const cookieScore = document.getElementById("cookie-score")
+const multiplierBtn = document.getElementById("multiplier")
+const multiplierScore = document.getElementById("multiplier-score")
 
 let score = 0
+let multiplier = 1
 
-function incrementScore(event) {
+async function incrementScore(event) {
     event.preventDefault();
-    score++;
-    cookieCount.textContent = score;
+    score+= 1 * multiplier;
+    cookieScore.textContent = score;
 }
 
+async function augmenterMultiplicateur(event) {
+    event.preventDefault();
+
+    if (score < 50) {
+        return false;
+    } else if (score >=50) {
+        multiplier++
+        multiplierScore.textContent = `x${multiplier}`
+        score-=50
+        cookieScore.textContent = score;
+    }
+}
+
+multiplierBtn.addEventListener('click', augmenterMultiplicateur)
 cookieBtn.addEventListener('click', incrementScore)
