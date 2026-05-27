6af96f0a2bdd2a45d33026b8a64059c7de0a81f2
Nassim <achnassim34@gmail.com>
2026-05-27 12:17:23 +0200

    <Feat> Autoclicker achetable fonctionnel

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const cookieBtn = document.getElementById("click")
 const cookieScore = document.getElementById("cookie-score")
 const multiplierBtn = document.getElementById("multiplier")
+const autoclickerBtn = document.getElementById("autoclic")
 
-let score = 0
+let score = 501
+cookieScore.textContent = score
 let multiplierCount = 1
 let multiplier = 1 * multiplierCount
+
+let bonus = {
+    "autoclic": {
+        "achete": false,
+        "cout": 500,
+        "desc": "L'autoclicker permet d'automatiquement cliquer sur le cookie toutes les secondes."
+    }
+}
+
+let autoclicDict = bonus["autoclic"]
+let autoclicCout = autoclicDict["cout"]
+let autoclicAchete = autoclicDict["achete"]
 
 async function incrementScore(event) {
     event.preventDefault();
@@ -26,5 +40,23 @@ async function augmenterMultiplicateur(event) {
     }
 }
 
+async function acheterAutoclicker(event) {
+    event.preventDefault();
+
+    if (autoclicCout > score) {
+        return false;
+    } else if (autoclicCout <= score && autoclicAchete == false) {
+        autoclicAchete = true;
+        score -= autoclicCout
+        autoclickerBtn.textContent = `Autoclicker (Acheté)`
+        cookieScore.textContent = score;
+        loop_autoclic = setInterval(function(){
+            score+= 1 * multiplier;
+            cookieScore.textContent = score;
+        }, 1000)
+    }
+}
+
 multiplierBtn.addEventListener('click', augmenterMultiplicateur)
 cookieBtn.addEventListener('click', incrementScore)
+autoclickerBtn.addEventListener('click', acheterAutoclicker)
